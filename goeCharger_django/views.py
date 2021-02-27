ccf53f0e8eec0aba242d5dd055c2c6413bdb82cc
Planet-Moon <topwastaken@abwesend.de>
2021-02-27 11:56:12 +0100

    [feature] add goe_charger variables

--- goeCharger_django/views.py
+++ goeCharger_django/views.py
@@ -4,7 +4,24 @@ from goeCharger import GOE_Charger as goeCharger
 def goe_index(request):
     goe_charger = goeCharger("http://192.168.178.106")
     context = {}
-    context["car"] = goe_charger.car
+    if int(goe_charger.car) > 1:
+        car = "connected"
+    else:
+        car = "not connected"
+    context["car"] = car
     context["amp"] = goe_charger.amp
-    context["alw"] = str(goe_charger.alw)
+    if goe_charger.alw:
+        alw = "charging"
+    else:
+        alw = "not charging"
+    context["alw"] = alw
+
+    if goe_charger.power_threshold < 0:
+        pwr_thresh = "not configured"
+        pwr_thresh_unit = ""
+    else:
+        pwr_thresh = str(goe_charger.power_threshold)
+        pwr_thresh_unit = "A"
+    context["min_amp"] = pwr_thresh
+    context["min_amp_unit"] = pwr_thresh_unit
     return render(request, 'goe_index.html', context)
