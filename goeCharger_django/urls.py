dc13306d993d6df471100b5b7d79a3d036c1fe60
Planet-Moon <topwastaken@abwesend.de>
2021-03-01 22:33:59 +0100

    [feature] add urls

--- goeCharger_django/urls.py
+++ goeCharger_django/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.goe_index, name='goeCharger_index'),
+    path('', views.goe_charger_index, name='goe_charger_index'),
+    path('charger/<title>/', views.goe_charger_detail, name='goe_charger_detail'),
+    path('category/<category>/', views.goe_charger_category, name='goe_charger_category'),
 ]
