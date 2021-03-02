7970d0f869790a9e07923a05f4b050c06319b746
Planet-Moon <topwastaken@abwesend.de>
2021-03-02 22:15:57 +0100

    [feature] add sending command via mqtt

--- goeCharger_django/static/js/mqtt.js
+++ goeCharger_django/static/js/mqtt.js
@@ -21,18 +21,16 @@ var port = 8000;
 var path = "/mqtt";
 var clientID = "server_mqtt_client";
 var topic = "/home_test_server/goe_charger";
-var client = new Paho.MQTT.Client(host,port,path,clientID);
+var mqttClient = new Paho.MQTT.Client(host,port,path,clientID);
 
 var Messages = new messagesHandle(5);
 
-console.log(Messages);
-
 // set callback handlers
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
+mqttClient.onConnectionLost = onConnectionLost;
+mqttClient.onMessageArrived = onMessageArrived;
 
 // connect the client
-client.connect({
+mqttClient.connect({
     onSuccess:onConnect
 });
 
@@ -40,9 +38,9 @@ client.connect({
 function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
     console.log("onConnect");
-    client.subscribe(topic+"/#");
+    mqttClient.subscribe(topic+"/#");
     payload = "First message";
-    client.send(topic, payload, qos=0, retained=true);
+    mqttClient.send(topic, payload, qos=0, retained=true);
 };
 
 // called when the client loses its connection
@@ -60,29 +58,41 @@ function onMessageArrived(message) {
     Messages.messages.forEach( element => {
         $("#messages-table").prepend("<tr><th>"+element.destinationName+"</th><th>"+element.payloadString+"</th></tr>");
     });
+    try{
+        receivedData = JSON.parse(message.payloadString);}
+    catch(e){}
+    console.log(receivedData);
 };
 
-$(document).ready(function() {
+$(document).ready(() => {
     var counter = 0;
-    setInterval(function() {
-        client.send(topic+"/subtopic", String(counter), qos=0, retained=true);
-        counter++;
-    }, 2000)
+    setInterval(() => {
+        if(mqttClient.isConnected()){
+            ret_val = mqttClient.send(topic+"/subtopic", String(counter), qos=0, retained=true);
+            console.log(ret_val);
+            counter++;
+        }
+    }, 10000)
 });
 
-$('#test-form').on('submit', (event) => {
+$('#test-form').on('submit', event => {
     event.preventDefault();
     input_text = $('#test-text').val();
     console.log("text ("+input_text+") submitted!");
     $("#return-text").html(input_text);
 });
 
-$('#toggle-charging-form').on('submit', (event) => {
+$('#toggle-charging-form').on('submit', event => {
     event.preventDefault();
+    var payload = {command:"enable","args":!charging_state}
+    payload = JSON.stringify(payload)
     if(charging_state){
         $("#btn-toggle-charging").html("Stop charging");
     }
     else{
         $("#btn-toggle-charging").html("Charge");
+    }
+    if(mqttClient.isConnected()){
+        mqttClient.send(topic+"/"+charger_name, payload, qos=0, retained=true)
     }
 });
