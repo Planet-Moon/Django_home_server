b950b568a082dd1ad9c4f2b671e688f6e6cc9540
Planet-Moon <topwastaken@abwesend.de>
2021-03-03 23:17:25 +0100

    [feature] add goe_charger instances

--- goeCharger_django/views.py
+++ goeCharger_django/views.py
@@ -5,6 +5,52 @@ from goeCharger_django.models import Car
 from goeCharger import GOE_Charger as goeCharger
 from goeCharger_django.forms import CarForm, TestForm
 
+import logging
+import threading
+import time
+
+logger = logging.getLogger(__name__)
+
+
+def reset_running():
+    chargers = GoeCharger_model.objects.all()
+    for charger in chargers:
+        charger.thread_running = False
+        charger.save()
+reset_running()
+
+class GoeCharger_thread(threading.Thread):
+    def __init__(self, goeCharger):
+        self.goeCharger = goeCharger
+        threading.Thread.__init__(self)
+
+    def run(self):
+        # self.goeCharger.stop_loop()
+        ipAddress = self.goeCharger.address.replace("http://","")
+        charger = GoeCharger_model.objects.get(title=self.goeCharger.name)
+        logging.info("charger: "+str(charger))
+        if charger:
+            logging.info("running: "+str(charger.thread_running))
+            if not charger.thread_running:
+                charger.thread_running = True
+                charger.save()
+                self.goeCharger.start_loop()
+
+goe_chargers_instances = []
+def create_goe_instances():
+    chargers = GoeCharger_model.objects.all()
+    for charger in chargers:
+        goe_charger_instance = goeCharger(
+            name=charger.title,
+            address="http://"+charger.ipAddress,
+            mqtt_topic="/home_test_server/goe_charger/"+charger.title,
+            mqtt_broker="192.168.178.107",
+            mqtt_port=1883)
+        goe_chargers_instances.append(goe_charger_instance)
+        GoeCharger_thread(goe_charger_instance).start()
+        time.sleep(1)
+create_goe_instances()
+
 
 def goe_charger_index(request):
     chargers = GoeCharger_model.objects.all().order_by("-created_on")
@@ -43,11 +89,9 @@ def goe_charger_detail(request,title):
 
     charger_data = {}
 
-    if True:
+    if False:
         try:
-            charger = goeCharger("http://"+goe_charger.ipAddress,"/home_test_server/goe_charger/"+title,"broker.hivemq.com",1883)
-
-
+            charger = None
             if int(charger.car) > 1:
                 car = "connected"
             else:
@@ -83,5 +127,5 @@ def goe_charger_detail(request,title):
         charger_data["min_amp"] = "not configured"
         charger_data["min_amp_unit"] = ""
 
-    context = {"charger":goe_charger, "charger_data":charger_data, "car_form":car_form, "test_form":test_form}
+    context = {"charger":goe_charger, "car_form":car_form, "test_form":test_form}
     return render(request, 'goe_charger_detail.html', context)
